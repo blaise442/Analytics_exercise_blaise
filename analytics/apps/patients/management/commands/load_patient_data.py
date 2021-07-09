a0bcdd0082de75f342eb848da752dc33adefae0a
Joel Watts <joel@decisiohealth.com>
2021-07-08 20:35:38 -0500

    Add management command to load data

---
+++ analytics/apps/patients/management/commands/load_patient_data.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Load patient data from CSV'
+
+    def handle(self, *args, **options):
+        # TODO: load patient data from CSV into models
