4e556c678fdcff2258e9d4ec552f041b9ea91c4f
Joel Watts <joel@decisiohealth.com>
2021-07-08 20:24:39 -0500

    Add skeleton Django project

---
+++ analytics/apps/patients/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+
+# TODO: create your models here.
---
+++ analytics/apps/patients/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+
+
+def query(request):
+    data = {}
+
+    # TODO: query the DB and return results
+
+    return JsonResponse(data)
